bc14b045fa6f86c4241f9891afe7ad156bdbd0ad
David Sent <david.sent@app-akademie.com>
2025-07-29 10:40:10 +0200

    Update app name and description in manifest and main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -11,6 +11,7 @@ class MainApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      title: 'Davids Aim Game',
       debugShowCheckedModeBanner: false,
       theme: ThemeData.from(colorScheme: ColorScheme.fromSeed(seedColor: Colors.blueGrey)),
       home: GameScreen(),
